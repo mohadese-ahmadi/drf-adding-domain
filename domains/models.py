972841cd6c43f4f4c73a5df2bbd79bb732c0026d
moha <m.ahmadi1600@gmail.com>
2025-09-21 12:18:45 +0330

    altering status column

--- domains/models.py
+++ domains/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Domain(models.Model):
     STATUS_CHOICE=[
-        ('pending','Pending'),
-        ('verified','Verified'),
-        ('rejected','Rejected'),
+        (1 ,'Pending'),
+        (2 ,'Verified'),
+        (3 ,'Rejected'),
     ]
     domain=models.CharField(max_length=50, unique=True)
     isActive=models.BooleanField(default=True)
-    status=models.CharField(choices=STATUS_CHOICE, max_length=10, default="pending",)
+    status=models.IntegerField(choices=STATUS_CHOICE, default=1,)
     createdDate=models.DateTimeField(auto_now_add=True)
